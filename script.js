df2ecb7736988f2d5ccaf8f6a52c6e65e3bf176d
SupritD <31881396+SupritD@users.noreply.github.com>
2022-09-06 01:50:11 +0530

    first commit with all files

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// (function(){
+//     document.getElementById("header").style.display = "none";
+//     document.getElementById("main").style.display = "none";
+// })();
+
+// const preloader = setTimeout(pre_fun, 2000);
+// function pre_fun(){
+//     document.getElementById("preload").style.opacity = "0";
+//     document.getElementById("preload").style.display = "none";
+//     document.getElementById("header").style.display = "block";
+//     document.getElementById("main").style.display = "flex";
+// };
+
+
+// (function(){
+//         document.getElementById("navigation").style.display = "none";
+//         document.getElementById("main").style.display = "none";
+//     })();
